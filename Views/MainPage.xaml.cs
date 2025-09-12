88070857ceb1d68c9c04b3efa0af4918ca20172e
Gebruiker <Gebruiker@WS1033>
2025-09-12 14:35:10 +0200

    Revert "fix"
    This reverts commit 3994112f6c402fc5c7a9798528441a438e349040.

--- Views/MainPage.xaml.cs
+++ Views/MainPage.xaml.cs
@@ -14,13 +14,13 @@ public partial class MainPage : ContentPage
     private async void OnScrollLeftClicked(object? sender, EventArgs e)
     {
         // prevent negative scroll
-        var newX = Math.Max(0, CategoryScroll.ScrollX - 400);
+        var newX = Math.Max(0, CategoryScroll.ScrollX - 200);
         await CategoryScroll.ScrollToAsync(newX, 0, true);
     }
 
     private async void OnScrollRightClicked(object sender, EventArgs e)
     {
-        var newX = CategoryScroll.ScrollX + 400;
+        var newX = CategoryScroll.ScrollX + 200;
         await CategoryScroll.ScrollToAsync(newX, 0, true);
     }
 }
