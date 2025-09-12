3994112f6c402fc5c7a9798528441a438e349040
Gebruiker <Gebruiker@WS1033>
2025-09-12 14:32:16 +0200

    fix

---
+++ Controls/CategoryButton.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.Maui.Controls;
+
+namespace MauiApp1.Controls
+{
+    public partial class CategoryButton : ContentView
+    {
+        public CategoryButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
